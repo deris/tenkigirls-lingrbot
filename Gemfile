1d1ea4b2fa9a7625ee8d35fe248c8cae580accd5
deris0126 <deris0126@gmail.com>
2013-05-31 00:54:44 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+gem 'json', '1.8.0'
+gem 'sinatra', '1.4.2'
---
+++ tenkigirls.rb
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+require 'json'
+require 'sinatra'
+
+load 'livedoorweather.rb'
+
+get '/' do
+  "lingr:TenkiGirls"
+end
+
+post '/' do
+  content_type :text
+  json = JSON.parse(request.body.string)
+  json["events"].select { |e| e["message"] }.map do |e|
+    case e["message"]["text"]
+    when /^(?:(今日|明日|明後日)の)?天気$/m
+      return (HELP % [$&, $1]) + girls_gobi
+    when /^天気地方リスト$/m
+      return LivedoorWether::get_supported_city.join(',')
+    when /^(?:(今日|明日|明後日)の(.+)|(.+)の(今日|明日|明後日))の天気$/m
+      date = $1 || $4
+      city = $2 || $3
+      tenki = LivedoorWether::get_weather_date(:city => city, :date => date, :only => :image)
+      return tenki['title'] unless tenki.nil?
+    when /^(.+)の天気$/m
+      return LivedoorWether::get_weather_summary(:city => $1)
+    else
+      # do nothing
+      return
+    end
+  end
+end
+
+HELP = 'XXXの%s で%s天気を教え'
+GIRLS_GOBI = [
+  'ます。べ、べつにあんたのために言ってるわけじゃないんだからね！',
+  'るアル',
+  'るイカ',
+  'るゲソ',
+  'るのじゃ',
+  'るっちゃ',
+  'るヨー',
+  'るですー',
+  'るですぅ',
+  'るにょろ',
+  'るですの',
+  'るですのー',
+  'るみゃ',
+  'るにゃ',
+  'るにゅ',
+  'るもん',
+  'るかな、かな',
+  'てア・ゲ・ル',
+  'ます。とミカサは報告します。',
+]
+
+def girls_gobi
+  return GIRLS_GOBI[rand GIRLS_GOBI.length]
+end
+
+
+__END__
+
+* help
+天気
+今日の天気
+明日の天気
+明後日の天気
+
+天気地方リスト
+
+* summary
+東京の天気
+
+* tenki
+
+明日の東京の天気
+東京の明日の天気
+
